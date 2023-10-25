aa79427239a1f2191b9d8ce1ae11d782cc4b06c3
Michael Ram <ramsamoojm@ufl.edu>
2023-10-25 14:06:08 -0400

    added decoder and loop

--- versioncontrol.py
+++ versioncontrol.py
@@ -8,7 +8,8 @@ def encoder(password):
     return encoded
 
 
-
+def decoder(encoded):
+    return encoded
 
 
 def main():
@@ -17,12 +18,18 @@ def main():
     print("1. Encode")
     print("2. Decode")
     print("3. Quit")
-    option = int(input("Please enter an option: "))
-    if option == 1:
-        password = input("Please enter your password to encode: ")
-        encoded_password = encoder(password)
-        print("Your password has been encoded and stored!")
 
+    while True:
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            encoded_password = encoder(password)
+            print("Your password has been encoded and stored!")
+        if option == 2:
+            decoded_password = encoded_password
+            print(f"The encoded password is {decoded_password}, and the original password is {password}.")
+        if option == 3:
+            break
 
 
 if __name__ == "__main__":
